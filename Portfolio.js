8bc04a32d485ccf326054f8b2fb025a3a504d045
chandu <chandrapilla2002@gmail.com>
2022-07-03 19:04:08 +0530

    Added some cool stuff

--- Portfolio.js
+++ Portfolio.js
@@ -1,6 +1,6 @@
-const firstSvg = [...document.getElementById("background").getElementsByClassName("first")]
-const secondSvg = [...document.getElementById("background").getElementsByClassName("second")]
-const thirdSvg = [...document.getElementById("background").getElementsByClassName("third")]
+const firstSvg = [...document.getElementById("background").getElementsByClassName("first")];
+const secondSvg = [...document.getElementById("background").getElementsByClassName("second")];
+const thirdSvg = [...document.getElementById("background").getElementsByClassName("third")];
 
 document.getElementById("header").addEventListener("mousemove", () => {
     firstSvg.forEach(path => {
@@ -60,4 +60,61 @@ window.onclick = function (click) {
         toggler.Inc()
         toggler.toggleClass('carretDown', 'fa-angle-down', 'fa-angle-right')
     }
+}
+
+const loadScreen = document.getElementById('loadScreen');
+const vectors = document.getElementById('vectors');
+const scoreHolder = document.getElementById('score');
+let score = 0;
+loadScreen.addEventListener('mousemove', (mouseData) => {
+    vectors.style.visibility = 'visible';
+    vectors.style.transform = `translateX(${mouseData.x}px) translateY(${mouseData.y}px)`;
+    const box1 = document.getElementById('box-1');
+    const randomPosX = Math.round(Math.random());
+    const randomPosY = Math.round(Math.random());
+    const vectorPos = vectors.style.transform.split(' ');
+    const boxPos = box1.style.transform.split(' ');
+    const VecPosX = parseInt(vectorPos[0].replaceAll(/\D/g, ''), 10);
+    const boxPosX = parseInt(boxPos[0].replaceAll(/\D/g, ''), 10);
+    const VecPosY = parseInt(vectorPos[1].replaceAll(/\D/g, ''), 10);
+    const boxPosY = parseInt(boxPos[1].replaceAll(/\D/g, ''), 10);
+
+    if ((VecPosX >= boxPosX && VecPosX < boxPosX + 50) && (VecPosY >= boxPosY && VecPosY < boxPosY + 50)) {
+        loadScreen.removeChild(box1);
+        const box = document.createElement('div')
+        box.id = 'box-1';
+        box.style.transform = `translateX(${loadScreen.offsetWidth / 2 + randomPosX * 100}px) translateY(${loadScreen.offsetHeight / 2 + randomPosY * 100}px)`;
+        box.className = 'boxes';
+        loadScreen.appendChild(box);
+        score++;
+        scoreHolder.innerText = `Score: ${score}`;
+    }
+})
+window.onload = function () {
+    //remove mousemove eventlistener from loadscreen
+    loadScreen.removeEventListener('mousemove', (mouseData) => {
+        console.log('removed')
+    })
+    loadScreen.innerHTML = `<p>Hurray! you scored ${score} points. Portfolio is loaded</p>`;
+    setTimeout(() => {
+        setInterval(hide, 10);
+        document.body.removeChild(loadScreen);
+    }, 1000);
+    // setInterval(hide, 10);
+
+    function hide() {
+        opacity = Number(loadScreen.style.opacity);
+        console.log(opacity)
+        if (opacity > 0) {
+            opacity = opacity - 0.1;
+            loadScreen.style.opacity = opacity
+        }
+        else {
+            clearInterval();
+        }
+    }
+    // document.body.removeChild(loadScreen);
+    // setTimeout(() => {
+    //     document.body.removeChild(loadScreen);
+    // }, 1000);
 }
